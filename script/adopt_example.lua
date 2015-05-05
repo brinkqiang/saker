5bd066aef89e73ad742d11b5d1a41dc69078c834
cinience <cinience@qq.com>
2015-05-05 17:52:24 +0800

    Add adopt example

--- script/adopt_example.lua
+++ script/adopt_example.lua
@@ -1,1 +1,10 @@
 --@see saker.supervisor
+
+---	local input = {}
+--- input['cmd'] = '/opt/app'
+--- input['args'] = {'-c=/etc/app.conf', '-s=1'}
+--- input['pidfile'] = '/var/run/app.pid'
+--- pid, err = saker.adopt(input) 
+--- if pid == nil then
+---     print(err)
+--- end
